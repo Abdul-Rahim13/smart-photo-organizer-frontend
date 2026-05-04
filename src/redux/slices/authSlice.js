f2e5bbf64b3bf6563af19e830923a821472e62b4
Abdul Rahim <raheemabdur106@gmail.com>
2026-05-05 01:47:46 +0500

    added loginUser thunk in slice

--- src/redux/slices/authSlice.js
+++ src/redux/slices/authSlice.js
@@ -8,7 +8,7 @@ export const registerUser = createAsyncThunk(
             const {name, email, password} = userData;
 
             if(!name || !email || !password) {
-                return rejectWithValue (" Name, email, and password are required")
+                return rejectWithValue ("Name, email, and password are required")
             }
 
             const res = await axios.post(
@@ -40,41 +40,113 @@ export const registerUser = createAsyncThunk(
     }
 )
 
+export const loginUser = createAsyncThunk(
+    
+    "auth/loginUser",
+    async (userData, {rejectWithValue}) => {
+        try {
+            const {email, password} = userData;
+
+            if(!email || !password) {
+                return rejectWithValue (" email and password required");
+            }
+
+            const res = await axios.post(
+                "https://smart-photo-organizer-backend-production.up.railway.app/api/auth/login",
+                { email, password },
+                {
+                headers: {
+                    "Content-Type": "application/json",
+                },
+                withCredentials: true, 
+                timeout: 10000,
+                }
+            );
+            
+            if(!res.data.success) {
+                throw new Error (res.data.message || "Login Failed");
+            }
+
+            return {
+                success: true,
+                message: res.data.message || "Login successful",
+                user: res.data.user || null,
+                token: res.data.token || null,
+            };
+
+        } catch (error) {
+            return rejectWithValue( error.response?.data?.message || error.message || "Something went wrong");
+        }
+    }
+
+)
+
 
 const authSlice = createSlice({
     name: "auth",
     initialState: {
         loading: false,
         error: null,
-        success: false,
+        registerSuccess: false,
+        loginSuccess: false,
         message: null,
+        user: null,
+        token: null,
     },
     reducers: {
         resetAuthState: (state) => {
             state.loading = false;
             state.error = null;
-            state.success = false;
+            state.loginSuccess = false;
+            state.registerSuccess = false;
             state.message = null;
+            state.user = null;
+            state.token = null;
         },
     },
 
     extraReducers: (builder) => {
         builder
+        // register User
         .addCase(registerUser.pending, (state) => {
-            state.loading = false;
+            state.loading = true;
             state.error = null;
-            state.success = false;
+            state.registerSuccess = false;
+            state.message = null;
         })
 
         .addCase(registerUser.fulfilled, (state, action) => {
-            state.loading = true;
-            state.success = true;
+            state.loading = false;
+            state.registerSuccess = true;
             state.message = action.payload.message;
         })
 
         .addCase(registerUser.rejected, (state, action) => {
         state.loading = false;
         state.error = action.payload;
+        state.registerSuccess = false;
+      })
+
+      // Login User
+      .addCase(loginUser.pending, (state) => {
+            state.loading = true;
+            state.error = null;
+            state.loginSuccess = false;
+            state.message = null;
+        })
+
+        .addCase(loginUser.fulfilled, (state, action) => {
+            state.loading = false;
+            state.loginSuccess = true;
+            state.message = action.payload.message;
+            state.user = action.payload.user;
+            state.token = action.payload.token;
+        })
+
+        .addCase(loginUser.rejected, (state, action) => {
+        state.loading = false;
+        state.error = action.payload;
+        state.loginSuccess = false;
       });
     }
 })
