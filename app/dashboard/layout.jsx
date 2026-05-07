d7ace04d207792b672c291824ac48b8534fff1bf
Abdul Rahim <raheemabdur106@gmail.com>
2026-05-07 23:41:13 +0500

    changes in lauout page

--- app/dashboard/layout.jsx
+++ app/dashboard/layout.jsx
@@ -15,7 +15,7 @@ export default function DashboardLayout({ children }) {
     const localToken =
       typeof window !== "undefined" ? localStorage.getItem("token") : null;
 
-    // NOT logged in → redirect
+    // NOT logged in 
     if (!token && !localToken) {
       toast.error("Please login to access dashboard");
       router.replace("/login");
